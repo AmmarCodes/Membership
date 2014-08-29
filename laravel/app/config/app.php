2dfce3e90639656ca4462aa3cf221bbd54e9d7e0
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-30 01:15:14 +0300

    Start using Confide for users management

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -190,6 +190,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Confide'         => 'Zizaco\Confide\Facade',
 
 	),
 
