edbd5098d683dfea3518840519b923bc6e265fc0
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-29 23:47:16 +0300

    Add gulp with necessary npm & bower files

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+'use strict';
+// generated on 2014-08-29 using generator-gulp-bootstrap 0.0.4
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.sass({errLogToConsole: true}))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('app/styles'))
+        .pipe($.size())
+        .pipe($.notify("Compilation complete."))
+        .pipe(livereload({ auto: false }));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.size());
+});
+
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets())
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        // .pipe(reload({stream:true, once:true}))
+        .pipe(livereload({ auto: false }))
+        .pipe($.size());
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['app/styles/main.css', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', ['html', 'images']);
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('app/styles'));
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            exclude: ['bootstrap-sass-official']
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', [], function () {
+	livereload.listen();
+    // watch for changes
+    gulp.watch(['app/*.html']).on('change', function(file) { livereload.changed(file.path) });
+
+    gulp.watch('app/styles/**/*.scss', ['styles']); //.on('change', function(file) { livereload.changed(file.path) });
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
+});
