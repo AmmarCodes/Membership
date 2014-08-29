49fb192b8b1be618a9ee43a0f812528c045e0ec1
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-30 00:50:00 +0300

    Add custom config file and use it for project title

--- laravel/app/views/master.blade.php
+++ laravel/app/views/master.blade.php
@@ -2,7 +2,7 @@
 <html class="no-js">
 <head>
 	<meta charset="utf-8">
-	<title>@yield('title', 'Membership')</title>
+	<title>@yield('title', Config::get('custom.title'))</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
